4a6a31edc98b2bca7b89915d9bdcf55be9d3b1a8
Pritam-99 <pritam@influxiqtech.com>
2023-08-03 13:03:16 +0530

    Resume

--- src/App.js
+++ src/App.js
@@ -74,6 +74,22 @@ experience.</li>
             </ul>
           </div>
         </div>
+        <div class="section__list-item">
+          <div class="left">
+            <div class="name">Indian Institute of Engineering Science and Technology</div>
+            <div class="addr">Howrah, West Bengal, India 711103</div>
+            <div class="duration">August 2012- May 2015</div>
+            
+          </div>
+          <div class="right">
+            <div class="name">Junior Research Fellow</div>
+            <ul>
+              <li>
+Conducted research studies which led to the design and development of a DST-DRC project funded by Govt. Of  India under the project head “ Design and Development Substrate Integrated Waveguide based device and components using Meta-Material for Ku-Band applications”</li>
+            
+            </ul>
+          </div>
+        </div>
 
       </div>
     </div>
@@ -82,24 +98,42 @@ experience.</li>
       <div class="section__list">
         <div class="section__list-item">
           <div class="left">
-            <div class="name">Sample Institute of technology</div>
-            <div class="addr">San Fr, CA</div>
-            <div class="duration">Jan 2011 - Feb 2015</div>
+            <div class="name">Maulana Abul Kalam Azad University of Technology</div>
+            <div class="addr">West Bengal, India 741249</div>
+            <div class="duration">Nov 2019-Present</div>
           </div>
           <div class="right">
-            <div class="name">Fr developer</div>
-            <div class="desc">did This and that</div>
+            <div class="name">PhD</div>
+            <div class="name">Department of Electronics and Communication Engineering</div>
+            <div class="desc">Title of Research: Non-Linear Control Approach for Human Limb Movement</div>
+            <div class="desc">Supervisor: Dr. Achintya Das ( KGEC) Co- Supervisor: Dr. Biswarup Neogi
+</div>
           </div>
         </div>
         <div class="section__list-item">
           <div class="left">
-            <div class="name">Akount</div>
-            <div class="addr">San Monica, CA</div>
-            <div class="duration">Jan 2011 - Feb 2015</div>
+            <div class="name">Netaji Subhash Engineering College</div>
+            <div class="addr">West Bengal, India  700152</div>
+            <div class="duration">Aug 2010 - July 2012</div>
           </div>
           <div class="right">
-            <div class="name">Fr developer</div>
-            <div class="desc">did This and that</div>
+            <div class="name">Master of Technology (M.Tech) </div>
+            <div class="desc">Department of Communication Engineering</div>
+            <div class="desc">Project: On the studies of Fractal UWB antenna combined with Quarter Wavelengh Slots For Band Notched Characteristics</div>
+            <div class="desc">Publication: Anirban Karmakar, Pritam Chakraborty, Roudra Ghatak, D.R Poddar ," A Compact Fractal UWB Antenna with Open Ended Quarter Wavelength Slot for Band Notched Characteristics" 10.1109/INDICON.2011.6139498</div>
+          </div>
+        </div>
+        <div class="section__list-item">
+          <div class="left">
+            <div class="name">Aryabhatta Institute Of Engineering And Management</div>
+            <div class="addr">Durgapur, West Bengal, India 713148</div>
+            <div class="duration">Aug 2006 - July 2010</div>
+          </div>
+          <div class="right">
+            <div class="name">Bachelor of Technology (B.Tech) </div>
+            <div class="desc">Department of Electronics and Communication Engineering</div>
+            <div class="desc">Project: On the design and studies of UWB antenna </div>
+           
           </div>
         </div>
 
@@ -107,23 +141,164 @@ experience.</li>
       
   </div>
      <div class="section">
-      <div class="section__title">Projects</div> 
+      <div class="section__title">List of Research Publications (Journal & Conferences)</div> 
        <div class="section__list">
-         <div class="section__list-item">
-           <div class="name">DSP</div>
-           <div class="text">I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow.</div>
-         </div>
-         
-         <div class="section__list-item">
-                    <div class="name">DSP</div>
-           <div class="text">I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow. <a href="/login">link</a>
-           </div>
-         </div>
+       <ol type="A">
+       <li>
+      <div class="section__list-item">
+           <div class="text">2022-03 Upper limb muscle grading for some daily life activity using surface electromyography (S-EMG) and Discrete Wavelet transform,  Journal Of Xidian University, Volume 17 – Issue 7 – July 23 (SCOPUS, UGC CARE). Doi.org/10.37896/jxu17.7/102</div>
+      </div>
+      </li> 
+      <li>
+      <div class="section__list-item">
+           <div class="text">2022-03 Global stability analysis of mechanical prosthetic finger adaptive control, Romanian Journal of Information Technology and Automatic Control, Vol. 32, No. 1, 33-44, 2022 ( ESCI, SCOPUS). https://doi.org/10.33436/v32i1y202203</div>
+      </div>
+      </li> 
+      <li>
+      <div class="section__list-item">
+           <div class="text">2021-12 Surface Electromyogram (S-EMG) Spectrogram Analysis of Human Arm Activity Towards Interpretability and Classification, International Conference on Trends in Electronics and Health Informatics (TEHI)-2021, PSIT Kanpur. https://link.springer.com/book/10.1007/978-981-16-8826-3</div>
+      </div>
+      </li> 
+       <li>
+      <div class="section__list-item">
+           <div class="text">2020-10 Knowledge Based database of Arm-Muscle and Activity Characterization during Load Pull Exercise using Diagnostic Electromyography (D-EMG) Signal,Cogent Engineering ( Taylor and francis), ( Accepted) DOI:10.1080/23311916.2020.1849942 (ESCI-Scopus)</div>
+      </div>
+      </li> 
+      <li>
+      <div class="section__list-item">
+           <div class="text">2018-06 Mechanical Prosthetic Arm Adaptive I-PD Control Model Using MIT Rule Towards Global Stability, J.Mech.Cont.& Math. Sci., Vol.-13, No.-2, May-June(2018) Pages 43-55. doi10.26782/jmcms.2018.06.0000</div>
+      </div>
+      </li> 
+      <li>
+      <div class="section__list-item">
+           <div class="text">2017-09 Robotic complex for the development of thick steeply-inclined coal seams and ore deposits, IOP Conference series: Earth and Environmental Science 84(2017) 012002, IOP Publishing, DOI:10.1088/1755- 1315/84/1/012002.</div>
+      </div>
+      </li> 
+      <li>
+      <div class="section__list-item">
+           <div class="text">2016-09 Introducing MIT rule towards Improvement of Adaptive Mechanical prosthetic arm control model, International conference on Advanced Computing, Networking and Informatics ,ICACNI-2016, NIT Rourkela, DOI:link.springer.com/content/pdf/bfm:978-981-10-3373-5/1.pdf</div>
+      </div>
+      </li> 
+      <li>
+      <div class="section__list-item">
+           <div class="text">2018-01 Book Chapter: Introducing MIT rule toward, Improvement of Adaptive Mechanical prosthetic arm control model, • Advs in Intelligent Syst., Computing, Vol. 518, Pankaj Kumar Sa et al: PROGRESS IN INTELLIGENT COMPUTING TECHNIQUES: THEORY, PRACTICE, AND APPLICATIONS, 978-981- 10-3372-8, 417761_1_En (38).</div>
+      </div>
+      </li> 
+      <li>
+      <div class="section__list-item">
+           <div class="text">2011-12 A Compact Fractal UWB Antenna with open-ended Quarter Wavelength Slot for Band Notch Characteristics, INDICON-2011, Hyderabad,IEEE Doi.10.1109/INDCON.2011.6139498.</div>
+      </div>
+      </li> 
+
+       </ol>
        </div>
     </div>
      <div class="section">
        <div class="section__title">Skills</div>
        <div class="skills">
+       <div class="skills__item">
+           <div class="left"><div class="name">
+             ReactJS</div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" checked/>
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" checked/>
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         <div class="skills__item">
+           <div class="left"><div class="name">
+             NodeJs
+             </div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" checked/>
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" checked/>
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         <div class="skills__item">
+           <div class="left"><div class="name">
+             MongoDb
+             </div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" checked/>
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" checked/>
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         <div class="skills__item">
+           <div class="left"><div class="name">
+             HTML
+             </div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" checked/>
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" checked/>
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         <div class="skills__item">
+           <div class="left"><div class="name">
+             CSS</div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" checked/>
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" checked/>
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
          <div class="skills__item">
            <div class="left"><div class="name">
              Javascript
@@ -135,7 +310,7 @@ experience.</li>
                           <input id="ck2" type="checkbox" checked/>
 
               <label for="ck2"></label>
-                         <input id="ck3" type="checkbox" />
+                         <input id="ck3" type="checkbox" checked/>
 
               <label for="ck3"></label>
                            <input id="ck4" type="checkbox" />
@@ -145,28 +320,10 @@ experience.</li>
 
            </div>
          </div>
+        
          
        </div>
-       <div class="skills__item">
-           <div class="left"><div class="name">
-             CSS</div></div>
-           <div class="right">
-                          <input  id="ck1" type="checkbox" checked/>
-
-             <label for="ck1"></label>
-                          <input id="ck2" type="checkbox" checked/>
-
-              <label for="ck2"></label>
-                         <input id="ck3" type="checkbox" />
-
-              <label for="ck3"></label>
-                           <input id="ck4" type="checkbox" />
-            <label for="ck4"></label>
-                          <input id="ck5" type="checkbox" />
-              <label for="ck5"></label>
-
-           </div>
-         </div>
+      
          
        </div>
      <div class="section">
@@ -175,7 +332,7 @@ experience.</li>
        </div>
        <div class="section__list">
          <div class="section__list-item">
-                  Football, programming.
+                  Reading Novels, Listen Music, Watching Movies
           </div>
        </div>
      </div>
