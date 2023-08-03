a156af1cc7ecb0ad5d007e8adfc75a97e98f6f14
Pritam-99 <pritam@influxiqtech.com>
2023-08-03 12:22:03 +0530

    Resume

--- src/App.js
+++ src/App.js
@@ -3,22 +3,186 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <div>
+<div class="container">
+  <div class="header">
+    <div class="full-name">
+      <span class="first-name">Pritam</span> 
+      <span class="last-name">Chakraborty</span>
     </div>
+    <div class="contact-info">
+      <span class="email">Email: </span>
+      <span class="email-val">pritamiiest9@gmail.com</span>
+      <br></br>
+      <span class="email">Office Email: </span>
+      <span class="email-val">pritam@influxiqtech.com</span>
+      
+      <span class="separator"></span>
+      <span class="phone">Phone: </span>
+      <span class="phone-val">9002586079</span>
+    </div>
+    
+    <div class="about">
+      <span class="position">Full Stack Developer </span>
+      <br></br>
+      <span class="desc">
+       Hello, I am an adaptable Professional with passion in FrontEnd Web Development. Backed with 5 years of experience in the domain, I am proficient in FrontEnd Web development tools ReacJS, HTML, CSS, Javascript. My Love for problem solving and proactive attitude has driven me to explore new technology and frameworks over the years, ensuring that I can deliver robust and user friendly application. Apart from that, I am actively involved in open source community, contributing to the projects that promotes accessibility and inclusivity in the technology. 
+      </span>
+    </div>
+  </div>
+   <div class="details">
+    <div class="section">
+      <div class="section__title">Experience</div>
+      <div class="section__list">
+        <div class="section__list-item">
+          <div class="left">
+            <div class="name">InfluxIq Tech Pvt. Ltd</div>
+            <div class="addr">Kalyani, West Bengal, India 741235</div>
+            <div class="duration">May 2022- Present</div>
+          </div>
+          <div class="right">
+            <div class="name">FrontEnd developer</div>
+            <ul>
+              <li>Collaborated with stakeholders during development processes to confirm
+creative proposals and design best practices.</li>
+              <li>Use regulatory and compliance standards for user interface designs.</li>
+              <li>Collaborating with designers and user experience experts to create an intuitive
+and efficient user interface that provides a smooth and enjoyable user
+experience.</li>
+              <li>Created responsive and mobile-friendly user interfaces.</li>
+            </ul>
+          </div>
+        </div>
+                <div class="section__list-item">
+          <div class="left">
+            <div class="name">St Mary Technical Campus Kolkata</div>
+            <div class="addr">Barasat, West Bengal, India 700124</div>
+            <div class="duration">May 2019- May 2022</div>
+            
+          </div>
+          <div class="right">
+            <div class="name">Assistant Professor</div>
+            <ul>
+              <li>Coordinated with colleagues in continuous reviews and curriculum development, ensuring students received high-quality courses.</li>
+              <li>
+              Continuously improved subject knowledge by conducting thorough research.
+              </li>
+              <li>
+              Delivered engaging lectures, seminars and tutorials, increasing class productivity.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="section">
+      <div class="section__title">Education</div>
+      <div class="section__list">
+        <div class="section__list-item">
+          <div class="left">
+            <div class="name">Sample Institute of technology</div>
+            <div class="addr">San Fr, CA</div>
+            <div class="duration">Jan 2011 - Feb 2015</div>
+          </div>
+          <div class="right">
+            <div class="name">Fr developer</div>
+            <div class="desc">did This and that</div>
+          </div>
+        </div>
+        <div class="section__list-item">
+          <div class="left">
+            <div class="name">Akount</div>
+            <div class="addr">San Monica, CA</div>
+            <div class="duration">Jan 2011 - Feb 2015</div>
+          </div>
+          <div class="right">
+            <div class="name">Fr developer</div>
+            <div class="desc">did This and that</div>
+          </div>
+        </div>
+
+      </div>
+      
+  </div>
+     <div class="section">
+      <div class="section__title">Projects</div> 
+       <div class="section__list">
+         <div class="section__list-item">
+           <div class="name">DSP</div>
+           <div class="text">I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow.</div>
+         </div>
+         
+         <div class="section__list-item">
+                    <div class="name">DSP</div>
+           <div class="text">I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow. <a href="/login">link</a>
+           </div>
+         </div>
+       </div>
+    </div>
+     <div class="section">
+       <div class="section__title">Skills</div>
+       <div class="skills">
+         <div class="skills__item">
+           <div class="left"><div class="name">
+             Javascript
+             </div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" />
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" />
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         
+       </div>
+       <div class="skills__item">
+           <div class="left"><div class="name">
+             CSS</div></div>
+           <div class="right">
+                          <input  id="ck1" type="checkbox" checked/>
+
+             <label for="ck1"></label>
+                          <input id="ck2" type="checkbox" checked/>
+
+              <label for="ck2"></label>
+                         <input id="ck3" type="checkbox" />
+
+              <label for="ck3"></label>
+                           <input id="ck4" type="checkbox" />
+            <label for="ck4"></label>
+                          <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+
+           </div>
+         </div>
+         
+       </div>
+     <div class="section">
+     <div class="section__title">
+       Interests
+       </div>
+       <div class="section__list">
+         <div class="section__list-item">
+                  Football, programming.
+          </div>
+       </div>
+     </div>
+     </div>
+  </div>
+</div>
+   </>
   );
 }
 
